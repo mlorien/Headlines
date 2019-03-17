5accedcb48512e6a3d57c84fb190877514c161b7
Lorien Moisyn <lorien.moisyn@auspex.com.br>
2019-03-17 13:40:35 -0300

    use urlCache to load images faster

--- Headlines/Headlines/Source/View/HeadlineCell.swift
+++ Headlines/Headlines/Source/View/HeadlineCell.swift
@@ -12,18 +12,17 @@ import UIKit
 class HeadlineCell: UITableViewCell {
     
     @IBOutlet var coloredView: UIView!
-//    @IBOutlet weak var imageView: !
+    @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet weak var alignTrailing: NSLayoutConstraint!
     @IBOutlet weak var alignLeading: NSLayoutConstraint!
     
     var titleText: String!
-    var titleColor: UIColor!
     var alignment: NSTextAlignment!
     var imageURL: String!
     
-    let oddColor = "1F2833".toUIColor()
-    let evenColor = "D1C6C7".toUIColor()
+    let darkBlue = "242D44".toUIColor()
+    let lightPink = "D1C6C7".toUIColor()
     
     override func didMoveToSuperview() {
         setup()
@@ -45,19 +44,35 @@ class HeadlineCell: UITableViewCell {
     
     func setupLayout() {
         titleLabel.text = titleText
-        titleLabel.textColor = titleColor
-        coloredView.backgroundColor = backgroundColor
+        titleLabel.textColor = lightPink
+        titleLabel.backgroundColor = UIColor.black.withAlphaComponent(0.15)
         if alignment == .left {
             alignTrailing.priority = .defaultLow
         } else {
             alignLeading.priority = .defaultLow
         }
+        guard imageURL != nil else { return }
+        setImage()
+    }
+    
+    fileprivate func setImage() {
+        URLSession.shared
+            .dataTask(with: URL(string: imageURL!)!) { (data, response, error) in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                let capacity = 500 * 1024 * 1024
+                let urlCache = URLCache(memoryCapacity: capacity, diskCapacity: capacity, diskPath: "diskPath")
+                URLCache.shared = urlCache
+                DispatchQueue.main.async {
+                    self.myImageView.image = UIImage(data: data!)
+                }
+            }.resume()
     }
     
     func setup(with headline: Headline, even: Bool) {
         titleText = headline.title.replacingOccurrences(of: " - ", with: "\n")
-        backgroundColor = even ? evenColor : oddColor
-        titleColor = even ? oddColor : evenColor
         alignment = even ? .right : .left
         imageURL = headline.urlToImage
     }
